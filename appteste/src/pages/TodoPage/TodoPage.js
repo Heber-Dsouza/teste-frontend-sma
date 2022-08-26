b1e67c392a486e1d2115e1aaa6ce0ad4791895ba
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-08-26 12:04:48 -0300

    Correções finais. Deploy final.

--- appteste/src/pages/TodoPage/TodoPage.js
+++ appteste/src/pages/TodoPage/TodoPage.js
@@ -10,16 +10,18 @@ import {
 } from "./styled"
 
 const message = "Seja bem-vindo(a) a página de anotações. Faça login para continuar ou crie um cadastro!"
+const message2 = "Parece que você ainda não tem anotações..."
 
 const TodoPage = () => {
 
-  const { userTodo } = useContext(GlobalStateContext)
+  const { userTodo, currentUserId } = useContext(GlobalStateContext)
 
   return (
     <ScreenContainer>
       <MainContainer>
 
-        {userTodo.length === 0 && <CustomWelcome>{message}</CustomWelcome>}
+        {userTodo.length === 0 && currentUserId === null && <CustomWelcome>{message}</CustomWelcome>}
+        {userTodo.length === 0 && currentUserId !== null && <CustomWelcome>{message2}</CustomWelcome>}
         <GridForTodo />
 
       </MainContainer>
